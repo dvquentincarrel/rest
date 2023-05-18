4335a15f8f4aed1b9751f50ab8c33745cc6519b9
dvquentincarrel <dvquentincarrel@gmail.com>
2023-05-18 21:48:26 +0200

    [M] every link is in a big table now, data is way easier to manipulate

--- sql/schema.sql
+++ sql/schema.sql
@@ -37,50 +37,76 @@ CREATE TABLE edition (
     isbn string
 );
 
-CREATE TABLE author_decade (
+--CREATE TABLE author_decade (
+--    author integer,
+--    decade integer,
+--    FOREIGN KEY (author) REFERENCES author(id),
+--    FOREIGN KEY (decade) REFERENCES decade(id),
+--    PRIMARY KEY (author, decade)
+--);
+--
+--CREATE TABLE author_decade_genre (
+--    author integer,
+--    decade integer,
+--    genre integer,
+--    FOREIGN KEY (author) REFERENCES author(id),
+--    FOREIGN KEY (genre) REFERENCES genre(id),
+--    FOREIGN KEY (decade) REFERENCES decade(id),
+--    PRIMARY KEY (genre, decade, author)
+--);
+--
+--CREATE TABLE author_decade_genre_piece (
+--    author integer,
+--    decade integer,
+--    genre integer,
+--    piece integer,
+--    FOREIGN KEY (author) REFERENCES author(id),
+--    FOREIGN KEY (genre) REFERENCES genre(id),
+--    FOREIGN KEY (decade) REFERENCES decade(id),
+--    FOREIGN KEY (piece) REFERENCES piece(id),
+--    PRIMARY KEY (genre, decade, author, piece)
+--);
+--
+--CREATE TABLE editor_collection (
+--    editor integer,
+--    collection integer,
+--    FOREIGN KEY (editor) REFERENCES editor(id),
+--    FOREIGN KEY (collection) REFERENCES collection(id),
+--    PRIMARY KEY (editor, collection)
+--);
+--
+--CREATE TABLE editor_collection_edition (
+--    editor integer,
+--    collection integer,
+--    edition integer,
+--    FOREIGN KEY (editor) REFERENCES editor(id),
+--    FOREIGN KEY (collection) REFERENCES collection(id),
+--    FOREIGN KEY (edition) REFERENCES edition(id),
+--    PRIMARY KEY (editor, collection, edition)
+--);
+--
+--CREATE TABLE piece_edition (
+--    piece integer,
+--    edition integer,
+--    FOREIGN KEY (piece) REFERENCES piece(id),
+--    FOREIGN KEY (edition) REFERENCES edition(id),
+--    PRIMARY KEY (piece, edition)
+--);
+
+CREATE TABLE links (
     author integer,
     decade integer,
-    FOREIGN KEY (author) REFERENCES author(id),
-    FOREIGN KEY (decade) REFERENCES decade(id),
-    PRIMARY KEY (author, decade)
-);
-
-CREATE TABLE genre_decade (
     genre integer,
-    decade integer,
-    FOREIGN KEY (genre) REFERENCES genre(id),
-    FOREIGN KEY (decade) REFERENCES decade(id),
-    PRIMARY KEY (genre, decade)
-);
-
-CREATE TABLE piece_genre (
-    piece integer,
-    genre integer,
-    FOREIGN KEY (piece) REFERENCES piece(id),
-    FOREIGN KEY (genre) REFERENCES genre(id),
-    PRIMARY KEY (piece, genre)
-);
-
-CREATE TABLE piece_edition (
-    piece integer,
-    edition integer,
-    FOREIGN KEY (piece) REFERENCES piece(id),
-    FOREIGN KEY (edition) REFERENCES edition(id),
-    PRIMARY KEY (piece, edition)
-);
-
-CREATE TABLE editor_collection (
-    editor integer,
     collection integer,
+    editor integer,
+    edition integer,
+    piece integer,
+    FOREIGN KEY (author) REFERENCES author(id),
+    FOREIGN KEY (genre) REFERENCES genre(id),
+    FOREIGN KEY (decade) REFERENCES decade(id),
+    FOREIGN KEY (piece) REFERENCES piece(id),
     FOREIGN KEY (editor) REFERENCES editor(id),
     FOREIGN KEY (collection) REFERENCES collection(id),
-    PRIMARY KEY (editor, collection)
-);
-
-CREATE TABLE collection_edition (
-    collection integer,
-    edition integer,
-    FOREIGN KEY (collection) REFERENCES collection(id),
     FOREIGN KEY (edition) REFERENCES edition(id),
-    PRIMARY KEY (collection, edition)
+    PRIMARY KEY (genre, decade, author, piece, editor, collection, edition)
 );
